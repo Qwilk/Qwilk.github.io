05edde94be72e322a39be1f0219de31687e01596
Nikita Tretyakov <qwinikita@gmail.com>
2020-06-29 22:50:24 +0300

    Qwilk
    Верстка + поправки

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ window.onload = function() {
         var now = new Date();
 
         var hourse = now.getHours();
-        if (hourse < 9) hourse = '0' + hourse;
+        if (hourse <= 9) hourse = '0' + hourse;
 
         var minutes = now.getMinutes();
-        if (minutes < 9) minutes = '0' + minutes; 
+        if (minutes <= 9) minutes = '0' + minutes; 
 
         var seconds = now.getSeconds();
-        if (seconds < 9) seconds = '0' + seconds; 
+        if (seconds <= 9) seconds = '0' + seconds; 
 
         document.getElementById('hourse').innerHTML = hourse;
         document.getElementById('minutes').innerHTML = minutes;
